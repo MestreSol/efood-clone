a49f7271a18b71bd29cef9046d7102ff3d86dceb
MestreSol <mb.muitobosta@gmail.com>
2024-12-17 16:05:58 -0300

    Update tag types in CardListHome and CardListPerfil components to 'Japonesa' for improved categorization

--- src/containers/CardListPerfil/index.tsx
+++ src/containers/CardListPerfil/index.tsx
@@ -65,7 +65,7 @@ const CardListPerfil = () => {
               'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!'
             )}
             rating="5"
-            tagType="Restaurante"
+            tagType="Japonesa"
             cover="https://s3-alpha-sig.figma.com/img/5d53/a2e2/28895a55942ffb290182da4c32ba645d?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPEGLq6mcmMI3jZ~MvxghfzN23FZTrqdy6mZk6h2yXosJmax1e29g4ibVblwccVKFD26OkqX5r2DVVp0XA6uRh1dLyqwMK8QJ6eVLHdrD1v5eVABTGt8436~yqerz6I6GSGjtQUHeU2W1te5PvVUdSbBAAIOf3SUxyUcl-zcY9loAe5PBJFslHiKTBhYi8NS3xkBfxBVRvbQ2r-w5hIeTim7Br-DUEsr23pmaS91r5oWAALLSQcMPrqlgkS080yztabkSKCqT5W339Ek2qkytz3~scJbrPS23DqsB7G4pdNMogyi8LI2nGpVEK2BtZU13mIyHT6985dS2sisV7vsUA__"
           />
           <Card
@@ -80,7 +80,7 @@ const CardListPerfil = () => {
               'Delicie-se com um café artesanal único e pratos leves preparados com carinho. Um ambiente aconchegante para relaxar e aproveitar momentos especiais. Visite-nos e sinta o sabor da tranquilidade!'
             )}
             rating="4.8"
-            tagType="Cafeteria"
+            tagType="Japonesa"
             cover="https://s3-alpha-sig.figma.com/img/5d53/a2e2/28895a55942ffb290182da4c32ba645d?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPEGLq6mcmMI3jZ~MvxghfzN23FZTrqdy6mZk6h2yXosJmax1e29g4ibVblwccVKFD26OkqX5r2DVVp0XA6uRh1dLyqwMK8QJ6eVLHdrD1v5eVABTGt8436~yqerz6I6GSGjtQUHeU2W1te5PvVUdSbBAAIOf3SUxyUcl-zcY9loAe5PBJFslHiKTBhYi8NS3xkBfxBVRvbQ2r-w5hIeTim7Br-DUEsr23pmaS91r5oWAALLSQcMPrqlgkS080yztabkSKCqT5W339Ek2qkytz3~scJbrPS23DqsB7G4pdNMogyi8LI2nGpVEK2BtZU13mIyHT6985dS2sisV7vsUA__"
           />
 
@@ -96,7 +96,7 @@ const CardListPerfil = () => {
               'Descubra peças exclusivas de moda e acessórios que destacam sua personalidade. Com design sofisticado e qualidade excepcional, seja elegante em qualquer ocasião. A elegância está ao seu alcance!'
             )}
             rating="4.5"
-            tagType="Loja de Moda"
+            tagType="Japonesa"
             cover="https://s3-alpha-sig.figma.com/img/5d53/a2e2/28895a55942ffb290182da4c32ba645d?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPEGLq6mcmMI3jZ~MvxghfzN23FZTrqdy6mZk6h2yXosJmax1e29g4ibVblwccVKFD26OkqX5r2DVVp0XA6uRh1dLyqwMK8QJ6eVLHdrD1v5eVABTGt8436~yqerz6I6GSGjtQUHeU2W1te5PvVUdSbBAAIOf3SUxyUcl-zcY9loAe5PBJFslHiKTBhYi8NS3xkBfxBVRvbQ2r-w5hIeTim7Br-DUEsr23pmaS91r5oWAALLSQcMPrqlgkS080yztabkSKCqT5W339Ek2qkytz3~scJbrPS23DqsB7G4pdNMogyi8LI2nGpVEK2BtZU13mIyHT6985dS2sisV7vsUA__"
           />
 
@@ -107,7 +107,7 @@ const CardListPerfil = () => {
             nameButton="Saiba Mais"
             to={`/perfil/4`}
             iconName={Star}
-            title="Conforto Lar"
+            title="Japonesa"
             description={getDescription(
               'Transforme sua casa em um lar com nossos móveis e decorações pensados para o seu bem-estar. Designs modernos, materiais de alta qualidade e conforto garantido. Encontre o estilo que é a sua cara!'
             )}
@@ -128,7 +128,7 @@ const CardListPerfil = () => {
               'Explore o mundo das plantas com nossa seleção incrível de flores, suculentas e ervas aromáticas. Tudo para trazer frescor, beleza e um toque verde à sua vida!'
             )}
             rating="4.9"
-            tagType="Jardinagem"
+            tagType="Japonesa"
             cover="https://s3-alpha-sig.figma.com/img/5d53/a2e2/28895a55942ffb290182da4c32ba645d?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPEGLq6mcmMI3jZ~MvxghfzN23FZTrqdy6mZk6h2yXosJmax1e29g4ibVblwccVKFD26OkqX5r2DVVp0XA6uRh1dLyqwMK8QJ6eVLHdrD1v5eVABTGt8436~yqerz6I6GSGjtQUHeU2W1te5PvVUdSbBAAIOf3SUxyUcl-zcY9loAe5PBJFslHiKTBhYi8NS3xkBfxBVRvbQ2r-w5hIeTim7Br-DUEsr23pmaS91r5oWAALLSQcMPrqlgkS080yztabkSKCqT5W339Ek2qkytz3~scJbrPS23DqsB7G4pdNMogyi8LI2nGpVEK2BtZU13mIyHT6985dS2sisV7vsUA__"
           />
 
@@ -144,7 +144,7 @@ const CardListPerfil = () => {
               'Aprenda música com os melhores! Aulas de diversos instrumentos, técnicas vocais e produção musical. Descubra seu talento e leve sua paixão pela música ao próximo nível.'
             )}
             rating="4.6"
-            tagType="Escola de Música"
+            tagType="Japonesa"
             cover="https://s3-alpha-sig.figma.com/img/5d53/a2e2/28895a55942ffb290182da4c32ba645d?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPEGLq6mcmMI3jZ~MvxghfzN23FZTrqdy6mZk6h2yXosJmax1e29g4ibVblwccVKFD26OkqX5r2DVVp0XA6uRh1dLyqwMK8QJ6eVLHdrD1v5eVABTGt8436~yqerz6I6GSGjtQUHeU2W1te5PvVUdSbBAAIOf3SUxyUcl-zcY9loAe5PBJFslHiKTBhYi8NS3xkBfxBVRvbQ2r-w5hIeTim7Br-DUEsr23pmaS91r5oWAALLSQcMPrqlgkS080yztabkSKCqT5W339Ek2qkytz3~scJbrPS23DqsB7G4pdNMogyi8LI2nGpVEK2BtZU13mIyHT6985dS2sisV7vsUA__"
           />
         </CardListContainer>
