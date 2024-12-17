566cf84c78318f1be2e27d7c8ca00856d1bcf8dc
MestreSol <mb.muitobosta@gmail.com>
2024-12-17 13:04:09 -0300

    Add global styles and theme configuration using styled-components

---
+++ src/globals/globalStyle.ts
@@ -0,0 +1,30 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import theme from './theme'
+
+const globalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #f0f0f5;
+  }
+`
+
+export default globalStyle
+
+export const Container = styled.div`
+  max-width: 1024px;
+  min-width: 375px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media screen and (max-width: ${theme.breakpoints.mobile}) {
+    display: flex;
+    flex-direction: column;
+    gap: 72px;
+  }
+`
